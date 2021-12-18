78fa72718a690e3e3e83227bd076eef5f36816d1
Eduardo Costa <ecosta@hubspot.com>
2021-12-18 18:40:49 +0000

    [llvm] preparing to move reusable code

--- inc/mal/prompt.hpp
+++ inc/mal/prompt.hpp
@@ -4,18 +4,24 @@
 #include <string>
 #include <type_traits>
 
-namespace mal::prompt {
+namespace mal::prompt::impl {
   static auto & read_user_input(std::string & line) {
     std::cout << "user> ";
     return std::getline(std::cin, line);
   }
+  template<typename Fn>
+  static auto run(Fn && rep, const std::string & line) {
+    return rep(line);
+  }
 
+}
+namespace mal::prompt {
   template<typename Fn>
   requires std::is_invocable_r_v<std::string, Fn, std::string>
   static void loop(Fn && rep) {
     while (std::cin) {
-      for (std::string line; read_user_input(line);) {
-        std::cout << rep(line) << "\n";
+      for (std::string line; impl::read_user_input(line);) {
+        std::cout << impl::run(rep, line) << "\n";
       }
     }
   }
