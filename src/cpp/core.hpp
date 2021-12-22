e192b7c271ee444388a77b8face9b876eb3125bb
Eduardo Costa <ecosta@hubspot.com>
2021-12-22 18:47:15 +0000

    [c++] list core functions

--- src/cpp/core.hpp
+++ src/cpp/core.hpp
@@ -50,6 +50,42 @@ namespace mal::core {
     return types::string { mal::str { os.str() } };
   }
 
+  static type is_empty(std::span<const type> args) noexcept {
+    if (args.empty()) return types::boolean { false };
+    if (args[0].is<types::list>()) {
+      const auto & list = (*args[0].as<types::list>()).peek();
+      return types::boolean { list.empty() };
+    }
+    if (args[0].is<types::vector>()) {
+      const auto & list = (*args[0].as<types::vector>()).peek();
+      return types::boolean { list.empty() };
+    }
+    return types::boolean { false };
+  }
+  static type is_list(std::span<const type> args) noexcept {
+    return types::boolean { !args.empty() && args[0].is<types::list>() };
+  }
+
+  static type count(std::span<const type> args) noexcept {
+    if (args.empty()) return types::number { 0 };
+    if (args[0].is<types::list>()) {
+      return types::number { static_cast<int>((*args[0].as<types::list>()).peek().size()) };
+    }
+    if (args[0].is<types::vector>()) {
+      return types::number { static_cast<int>((*args[0].as<types::vector>()).peek().size()) };
+    }
+    return types::number { 0 };
+  }
+
+  static type list(std::span<const type> args) noexcept {
+    mal::list<type> res {};
+    for (const auto & a : args) {
+      res = res + a;
+    }
+
+    return types::list { std::move(res) };
+  }
+
   static void setup_step2_funcs(auto & e) {
     e.set("+", types::lambda { details::int_bifunc(std::plus<>()) });
     e.set("-", types::lambda { details::int_bifunc(std::minus<>()) });
@@ -59,6 +95,11 @@ namespace mal::core {
 
   static void setup_step4_funcs(auto & e) {
     setup_step2_funcs(e);
+    e.set("list", types::lambda { list });
+    e.set("list?", types::lambda { is_list });
+    e.set("empty?", types::lambda { is_empty });
+    e.set("count", types::lambda { count });
+
     e.set("pr-str", types::lambda { pr_str });
     e.set("str", types::lambda { str });
     e.set("prn", types::lambda { details::cout_pr_str(true) });
