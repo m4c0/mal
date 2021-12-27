5ae0ac427d9ede8f386434073c75537bfbb1cb71
Eduardo Costa <ecosta@hubspot.com>
2021-12-27 15:31:15 +0000

    [c++] vec

--- src/cpp/core7.hpp
+++ src/cpp/core7.hpp
@@ -19,10 +19,18 @@ namespace mal::core {
     return types::list { std::move(res) };
   }
 
+  static type vec(std::span<const type> args) noexcept {
+    if (args.size() != 1) return types::error { "vec needs an interable" };
+    if (args[0].is<types::vector>()) return args[0];
+    if (!args[0].is<types::list>()) return types::error { "vec can't work on this" };
+    return types::vector { args[0].to_iterable() };
+  }
+
   static void setup_step7_funcs(auto rep, auto & e) {
     setup_step6_funcs(rep, e);
 
     e->set("cons", types::lambda { cons });
     e->set("concat", types::lambda { concat });
+    e->set("vec", types::lambda { vec });
   }
 }
