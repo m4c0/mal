8bc16a029aa34d419f6a5d77d802d3d1a2a87fab
Eduardo Costa <ecosta@hubspot.com>
2021-12-27 15:24:43 +0000

    [c++] quasiquote passing hard tests

--- src/cpp/eval.quasiquote.hpp
+++ src/cpp/eval.quasiquote.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "log.hpp"
+#include "printer.hpp"
 #include "types.hpp"
 
 #include <vector>
@@ -20,15 +22,17 @@ namespace mal::evals::impl {
     const type cons = types::symbol { "cons" };
 
     type res = types::list { std::vector<type> {} };
-    for (const auto & elt : ast_items) {
+    // NOLINTNEXTLINE since ranges are not available in my setup
+    for (auto elt_it = ast_items.rbegin(); elt_it != ast_items.rend(); ++elt_it) {
+      const auto & elt = *elt_it;
       if (elt.is_iterable()) {
         auto elt_items = elt.to_iterable();
         if (elt_items.size() == 2 && elt_items[0].to_symbol() == "splice-unquote") {
           res = types::list { concat, elt_items[1], res };
-        } else {
-          res = types::list { cons, quasiquote(elt), res };
+          continue;
         }
       }
+      res = types::list { cons, quasiquote(elt), res };
     }
     return res;
   }
@@ -37,6 +41,8 @@ namespace mal::evals {
   [[nodiscard]] static type quasiquote(const types::list & in) noexcept {
     std::span<const type> list = *in;
     if (list.size() != 2) return types::error { "quasiquote requires a single argument" };
-    return impl::quasiquote(in.at(1));
+    auto res = impl::quasiquote(in.at(1));
+    log::debug() << "quasiquote: " << pr_str(res) << "\n";
+    return res;
   }
 }
