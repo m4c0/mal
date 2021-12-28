b4c6d0ec1e18c0269d1614d7696e5a091591d3f1
Eduardo Costa <ecosta@hubspot.com>
2021-12-28 19:31:13 +0000

    [c++] apply

--- src/cpp/core9.hpp
+++ src/cpp/core9.hpp
@@ -8,8 +8,12 @@ namespace mal::core {
   static type throw_(std::span<const type> args) noexcept {
     return types::error { args[0] };
   }
-  static type apply(std::span<const type> args) noexcept {
-    return {};
+  static types::details::lambda_ret_t apply(std::span<const type> args, const std::shared_ptr<env> & e) noexcept {
+    if (args.size() < 2) return { {}, err("apply requires at least two arguments") };
+    if (!args[0].is<types::lambda>()) return { {}, err("apply require a function as first argument") };
+    if (!args.back().is_iterable()) return { {}, err("apply require a list/vector as last argument") };
+
+    return { e, types::list { args[0], args.subspan(1, args.size() - 2), args.back().to_iterable() } };
   }
   static type map(std::span<const type> args) noexcept {
     return {};
@@ -19,7 +23,7 @@ namespace mal::core {
     setup_step8_funcs(rep, e);
 
     e->set("throw", types::lambda { throw_ });
-    e->set("apply", types::lambda { apply });
+    e->set("apply", types::lambda { 0, apply });
     e->set("map", types::lambda { map });
   }
 }
