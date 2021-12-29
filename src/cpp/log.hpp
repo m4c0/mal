2d0208bd18f607347eb5a26f880fd6f220f35951
Eduardo Costa <ecosta@hubspot.com>
2021-12-29 14:52:27 +0000

    [c++] dummy logging for faster release version

--- src/cpp/log.hpp
+++ src/cpp/log.hpp
@@ -3,7 +3,20 @@
 #include <iostream>
 
 namespace mal::log {
+#ifdef NDEBUG
+  struct dummy {
+    template<typename Tp>
+    constexpr dummy operator<<(Tp && /*n*/) const noexcept {
+      return {};
+    }
+  };
+
+  static auto debug() noexcept {
+    return dummy {};
+  }
+#else
   static auto & debug() noexcept {
     return std::cerr;
   }
+#endif
 }
