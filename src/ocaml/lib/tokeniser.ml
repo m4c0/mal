7d447d52621c37c84be64c288ab3a4380eb025e7
Eduardo Costa <ecosta@hubspot.com>
2022-01-23 19:12:51 +0000

    [ocaml] tokenise with Seq

--- src/ocaml/lib/tokeniser.ml
+++ src/ocaml/lib/tokeniser.ml
@@ -1,39 +1,62 @@
 exception Eof
 
-let rec skip_comment s =
-  match s with
-  | [] -> []
-  | '\n' :: xs -> xs
-  | _ :: xs -> skip_comment xs
+type token = 
+  | Blank
+  | String of string
+  | Symbol of string
 
-let rec next s =
-  let rec read_string c s =
-    match s with
-    | [] -> raise Eof
-    | '"' :: xs -> (c ^ "\"") :: next xs
-    | '\\' :: x :: xs -> read_string (c ^ "\\" ^ String.make 1 x) xs
-    | x :: xs -> read_string (c ^ String.make 1 x) xs
-  in
-  let rec read_seq c s =
-    match s with
-    | [] -> c :: []
-    | x :: xs ->
-        match x with
-        | '~' | '@'
-        | ' ' | ',' | '\012' | '\n' | '\r' | '\t' 
-        | '[' | ']' | '{' | '}' | '(' | ')'
-        | '"' 
-        | ';'  -> c :: next s
-        | x -> read_seq (c ^ String.make 1 x) xs
-  in
+let rec take_comment s =
+  let open Seq in
+  match s() with
+  | Nil -> (Blank, s)
+  | Cons('\n', xs) -> (Blank, xs)
+  | Cons(_, xs) -> take_comment xs 
 
-  match s with
-  | [] -> []
-  | '~' :: '@' :: xs -> "~@" :: next xs
-  | x :: xs ->
-      match x with
-      | ' ' | ',' | '\012' | '\n' | '\r' | '\t' -> next xs
-      | '[' | ']' | '{' | '}' | '(' | ')' | '~' | '@' -> (String.make 1 x) :: next xs
-      | '"' -> read_string "\"" xs
-      | ';' -> xs |> skip_comment |> next
-      | x -> read_seq (String.make 1 x) xs
+let rec take_spaces s =
+  let open Seq in
+  match s() with
+  | Cons((' ' | ',' | '\t' | '\r' | '\n'), xs) -> take_spaces xs
+  | _ -> (Blank, s)
+
+let rec take_string acc s =
+  let open Seq in
+  match s() with
+  | Nil -> raise Eof
+  | Cons('\n', _) -> raise Eof
+  | Cons('"', xs) -> (String(acc ^ "\""), xs)
+  | Cons('\\', xs) -> (
+      match xs() with
+      | Nil -> raise Eof
+      | Cons(x, xx) -> take_string (acc ^ "\\" ^ (String.make 1 x)) xx
+  )
+  | Cons(x, xs) -> take_string (acc ^ (String.make 1 x)) xs
+
+let rec take_symbol acc s =
+  let open Seq in
+  match s() with
+  | Nil
+  | Cons(('[' | ']' | '{' | '}' | '(' | ')' | '\'' | '`' | '^'), _) -> (Symbol(acc), s)
+  | Cons(x, xs) -> take_symbol (acc ^ (String.make 1 x)) xs
+
+let take_unquote s =
+  let open Seq in
+  match s() with
+  | Cons('@', xs) -> (Symbol("~@"), xs)
+  | _ -> (Symbol("~"), s)
+
+let token_fn s =
+  let open Seq in
+  match s() with
+  | Nil -> None
+  | Cons(';', xs) -> Some(take_comment xs)
+  | Cons('~', xs) -> Some(take_unquote xs)
+  | Cons('"', xs) -> Some(take_string "\"" xs)
+  | Cons((' ' | ',' | '\t' | '\r' | '\n'), xs) -> Some(take_spaces xs)
+  | Cons(('[' | ']' | '{' | '}' | '(' | ')' | '\'' | '`' | '^') as x, xs)
+    -> Some(Symbol(String.make 1 x), xs)
+  | Cons(x, xs) -> Some(take_symbol (String.make 1 x) xs)
+
+let tokenise str =
+  str
+  |> String.to_seq
+  |> Seq.unfold token_fn
