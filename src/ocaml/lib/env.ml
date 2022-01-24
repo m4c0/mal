7a8bf5097d9ce9f061a9cd793b4ce051cdd2c7ca
Eduardo Costa <ecosta@hubspot.com>
2022-01-24 18:00:47 +0000

    [ocaml] "mutable" env

--- src/ocaml/lib/env.ml
+++ src/ocaml/lib/env.ml
@@ -7,21 +7,20 @@ type t = {
   map: Types.t Map.t
 }
 
-let create outer map = { outer; map }
-
-let of_list x =
-  x
-  |> List.to_seq
-  |> Map.of_seq
-  |> create None
-
-let find env key = 
+let rec find env key = 
   try Map.find key env.map
-  with Not_found -> raise (Unknown_symbol key)
+  with Not_found ->
+    match env.outer with
+    | None -> raise (Unknown_symbol key)
+    | Some(e) -> find e key
+
+let set key value env =
+  let mapper = fun _ -> Some(value) in
+  { outer = env.outer; map = Map.update key mapper env.map }
 
 let step2 =
-  [ ("+", Core.sum);
-    ("-", Core.sub);
-    ("*", Core.mult);
-    ("/", Core.div);
-  ] |> of_list
+  { outer = None; map = Map.empty }
+    |> set "+" Core.sum
+    |> set "-" Core.sub
+    |> set "*" Core.mult
+    |> set "/" Core.div
